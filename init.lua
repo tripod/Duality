d1f1d9d8f5f7f65265f3d5ed80088a37ab49e439
Mark Hahl <mark@dotrb.net>
2011-06-15 00:34:04 +1000

    Changed directory struct

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ elf.AddGuiObject(gui, lab)
 
 
 -- -- create the elf logo
-elflogotex = elf.CreateTextureFromFile("resources/background.jpg")
+elflogotex = elf.CreateTextureFromFile("resources/menu/background.jpg")
 -- 
 pic = elf.CreatePicture("ElfLogo")
 elf.SetPictureTexture(pic, elflogotex)
@@ -21,9 +21,9 @@ elf.AddGuiObject(gui, pic)
 
 
 -- add new game button
-nbtexoff = elf.CreateTextureFromFile("resources/new_off.png")
-nbtexover = elf.CreateTextureFromFile("resources/new_hover.png")
-nbtexon = elf.CreateTextureFromFile("resources/new_off.png")
+nbtexoff = elf.CreateTextureFromFile("resources/menu/new_off.png")
+nbtexover = elf.CreateTextureFromFile("resources/menu/new_hover.png")
+nbtexon = elf.CreateTextureFromFile("resources/menu/new_off.png")
 
 nb = elf.CreateButton("NewButton")
 elf.SetButtonOffTexture(nb, nbtexoff)
@@ -33,9 +33,9 @@ elf.SetGuiObjectPosition(nb, 50, elf.GetWindowHeight() - 350)
 elf.AddGuiObject(gui, nb)
 
 -- add new options button
-obtexoff = elf.CreateTextureFromFile("resources/options_off.png")
-obtexover = elf.CreateTextureFromFile("resources/options_hover.png")
-obtexon = elf.CreateTextureFromFile("resources/options_off.png")
+obtexoff = elf.CreateTextureFromFile("resources/menu/options_off.png")
+obtexover = elf.CreateTextureFromFile("resources/menu/options_hover.png")
+obtexon = elf.CreateTextureFromFile("resources/menu/options_off.png")
 
 ob = elf.CreateButton("OptionsButton")
 elf.SetButtonOffTexture(ob, obtexoff)
@@ -46,9 +46,9 @@ elf.AddGuiObject(gui, ob)
 
 
 -- add new about button
-abtexoff = elf.CreateTextureFromFile("resources/about_off.png")
-abtexover = elf.CreateTextureFromFile("resources/about_hover.png")
-abtexon = elf.CreateTextureFromFile("resources/about_off.png")
+abtexoff = elf.CreateTextureFromFile("resources/menu/about_off.png")
+abtexover = elf.CreateTextureFromFile("resources/menu/about_hover.png")
+abtexon = elf.CreateTextureFromFile("resources/menu/about_off.png")
 
 ab = elf.CreateButton("AboutButton")
 elf.SetButtonOffTexture(ab, abtexoff)
@@ -58,9 +58,9 @@ elf.SetGuiObjectPosition(ab, 50, elf.GetWindowHeight() - 250)
 elf.AddGuiObject(gui, ab)
 
 -- add quit about button
-qbtexoff = elf.CreateTextureFromFile("resources/quit_off.png")
-qbtexover = elf.CreateTextureFromFile("resources/quit_hover.png")
-qbtexon = elf.CreateTextureFromFile("resources/quit_off.png")
+qbtexoff = elf.CreateTextureFromFile("resources/menu/quit_off.png")
+qbtexover = elf.CreateTextureFromFile("resources/menu/quit_hover.png")
+qbtexon = elf.CreateTextureFromFile("resources/menu/quit_off.png")
 
 qb = elf.CreateButton("QuitButton")
 elf.SetButtonOffTexture(qb, qbtexoff)
@@ -76,10 +76,11 @@ elf.AddGuiObject(gui, qb)
 
 
 --load some music
-music = elf.LoadSound("resources/menu.ogg")
-sound_source = elf.PlaySound(music, 1.0)
+music = elf.LoadSound("resources/sound/menu.ogg")
+sound_source = elf.PlaySound(music, 0.5)
 
-elf.LoadScene("resources/menu.pak")
+-- Broken ;_;
+--elf.LoadScene("resources/menu.pak")
 
 while elf.Run() == true do
    elf.SetLabelText(lab, "FPS: " .. elf.GetFps())

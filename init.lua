b31034e2341a0b2276c236f1cad1d4cddd62188e
Mark Hahl <mark@dotrb.net>
2011-06-15 00:30:21 +1000

    Fixed background

--- init.lua
+++ init.lua
@@ -1,23 +1,24 @@
 gui = elf.CreateGui()
 elf.SetGui(gui)
 
--- create the elf logo
-elflogotex = elf.CreateTextureFromFile("resources/background.jpg")
-
-pic = elf.CreatePicture("ElfLogo")
-elf.SetPictureTexture(pic, elflogotex)
-elf.SetGuiObjectPosition(pic, 0 , 0 )
-elf.AddGuiObject(gui, pic)
-
-
 -- add a label
 font = elf.CreateFontFromFile("resources/DejaVuSans.ttf", 12)
 
 lab = elf.CreateLabel("FPSLabel")
 elf.SetLabelFont(lab, font)
-elf.SetLabelText(lab, "FPS: 0")
+elf.SetLabelText(lab, "Loading ...")
 elf.SetGuiObjectPosition(lab, 10, 10)
 elf.AddGuiObject(gui, lab)
+
+
+-- -- create the elf logo
+elflogotex = elf.CreateTextureFromFile("resources/background.jpg")
+-- 
+pic = elf.CreatePicture("ElfLogo")
+elf.SetPictureTexture(pic, elflogotex)
+elf.SetGuiObjectPosition(pic, 0 , 0 )
+elf.AddGuiObject(gui, pic)
+
 
 -- add new game button
 nbtexoff = elf.CreateTextureFromFile("resources/new_off.png")
@@ -28,7 +29,7 @@ nb = elf.CreateButton("NewButton")
 elf.SetButtonOffTexture(nb, nbtexoff)
 elf.SetButtonOverTexture(nb, nbtexover)
 elf.SetButtonOnTexture(nb, nbtexon)
-elf.SetGuiObjectPosition(nb, 50, 450)
+elf.SetGuiObjectPosition(nb, 50, elf.GetWindowHeight() - 350)
 elf.AddGuiObject(gui, nb)
 
 -- add new options button
@@ -40,7 +41,7 @@ ob = elf.CreateButton("OptionsButton")
 elf.SetButtonOffTexture(ob, obtexoff)
 elf.SetButtonOverTexture(ob, obtexover)
 elf.SetButtonOnTexture(ob, obtexon)
-elf.SetGuiObjectPosition(ob, 50, 500)
+elf.SetGuiObjectPosition(ob, 50, elf.GetWindowHeight() - 300)
 elf.AddGuiObject(gui, ob)
 
 
@@ -53,7 +54,7 @@ ab = elf.CreateButton("AboutButton")
 elf.SetButtonOffTexture(ab, abtexoff)
 elf.SetButtonOverTexture(ab, abtexover)
 elf.SetButtonOnTexture(ab, abtexon)
-elf.SetGuiObjectPosition(ab, 50, 550)
+elf.SetGuiObjectPosition(ab, 50, elf.GetWindowHeight() - 250)
 elf.AddGuiObject(gui, ab)
 
 -- add quit about button
@@ -65,7 +66,7 @@ qb = elf.CreateButton("QuitButton")
 elf.SetButtonOffTexture(qb, qbtexoff)
 elf.SetButtonOverTexture(qb, qbtexover)
 elf.SetButtonOnTexture(qb, qbtexon)
-elf.SetGuiObjectPosition(qb, 50, 600)
+elf.SetGuiObjectPosition(qb, 50, elf.GetWindowHeight() - 200)
 
 exscr = elf.CreateScript("Exit")
 elf.SetScriptText(exscr, "elf.Quit()")
@@ -74,10 +75,11 @@ elf.SetGuiObjectScript(qb, exscr)
 elf.AddGuiObject(gui, qb)
 
 
--- load some music
+--load some music
 music = elf.LoadSound("resources/menu.ogg")
 sound_source = elf.PlaySound(music, 1.0)
 
+elf.LoadScene("resources/menu.pak")
 
 while elf.Run() == true do
    elf.SetLabelText(lab, "FPS: " .. elf.GetFps())

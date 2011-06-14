e7c105df767ed41a0a4856ff57377fdbf55b8704
Mark Hahl <mark@dotrb.net>
2011-06-14 18:42:50 +1000

    Initial initial import ;)

---
+++ init.lua
@@ -0,0 +1,78 @@
+gui = elf.CreateGui()
+elf.SetGui(gui)
+
+-- create the elf logo
+elflogotex = elf.CreateTextureFromFile("resources/background.jpg")
+
+pic = elf.CreatePicture("ElfLogo")
+elf.SetPictureTexture(pic, elflogotex)
+elf.SetGuiObjectPosition(pic, 0 , 0 )
+elf.AddGuiObject(gui, pic)
+
+
+-- add a label
+font = elf.CreateFontFromFile("resources/DejaVuSans.ttf", 12)
+
+lab = elf.CreateLabel("FPSLabel")
+elf.SetLabelFont(lab, font)
+elf.SetLabelText(lab, "FPS: 0")
+elf.SetGuiObjectPosition(lab, 10, 10)
+elf.AddGuiObject(gui, lab)
+
+-- add new game button
+nbtexoff = elf.CreateTextureFromFile("resources/new_off.png")
+nbtexover = elf.CreateTextureFromFile("resources/new_hover.png")
+nbtexon = elf.CreateTextureFromFile("resources/new_off.png")
+
+nb = elf.CreateButton("NewButton")
+elf.SetButtonOffTexture(nb, nbtexoff)
+elf.SetButtonOverTexture(nb, nbtexover)
+elf.SetButtonOnTexture(nb, nbtexon)
+elf.SetGuiObjectPosition(nb, 50, 450)
+elf.AddGuiObject(gui, nb)
+
+-- add new options button
+obtexoff = elf.CreateTextureFromFile("resources/options_off.png")
+obtexover = elf.CreateTextureFromFile("resources/options_hover.png")
+obtexon = elf.CreateTextureFromFile("resources/options_off.png")
+
+ob = elf.CreateButton("OptionsButton")
+elf.SetButtonOffTexture(ob, obtexoff)
+elf.SetButtonOverTexture(ob, obtexover)
+elf.SetButtonOnTexture(ob, obtexon)
+elf.SetGuiObjectPosition(ob, 50, 500)
+elf.AddGuiObject(gui, ob)
+
+
+-- add new about button
+abtexoff = elf.CreateTextureFromFile("resources/about_off.png")
+abtexover = elf.CreateTextureFromFile("resources/about_hover.png")
+abtexon = elf.CreateTextureFromFile("resources/about_off.png")
+
+ab = elf.CreateButton("AboutButton")
+elf.SetButtonOffTexture(ab, abtexoff)
+elf.SetButtonOverTexture(ab, abtexover)
+elf.SetButtonOnTexture(ab, abtexon)
+elf.SetGuiObjectPosition(ab, 50, 550)
+elf.AddGuiObject(gui, ab)
+
+-- add quit about button
+qbtexoff = elf.CreateTextureFromFile("resources/quit_off.png")
+qbtexover = elf.CreateTextureFromFile("resources/quit_hover.png")
+qbtexon = elf.CreateTextureFromFile("resources/quit_off.png")
+
+qb = elf.CreateButton("QuitButton")
+elf.SetButtonOffTexture(qb, qbtexoff)
+elf.SetButtonOverTexture(qb, qbtexover)
+elf.SetButtonOnTexture(qb, qbtexon)
+elf.SetGuiObjectPosition(qb, 50, 600)
+
+exscr = elf.CreateScript("Exit")
+elf.SetScriptText(exscr, "elf.Quit()")
+elf.SetGuiObjectScript(qb, exscr)
+
+elf.AddGuiObject(gui, qb)
+
+while elf.Run() == true do
+   elf.SetLabelText(lab, "FPS: " .. elf.GetFps())
+end

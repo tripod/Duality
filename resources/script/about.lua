047fcc70e7907cdfaf7405939dff177713b5a231
Mark Hahl <Wolskie@hotmail.com>
2011-07-07 21:47:15 -0700

    Updated comments.

--- resources/script/about.lua
+++ resources/script/about.lua
@@ -1,6 +1,7 @@
+-- Play menu click sound on menu preload.
 PlaySound(menu_click, 0.5)
 
--- I dont know how to append to a  string
+-- TODO: Each person shoubd be appended to the string in a differnt line of code.
 abouttext = "Duality, a FPS style game, powered by the blendelf game engine.\n\nProgramming - Mark 'Ling' Hahl\nGraphics - James 'tripod' Gray\nMusic - Mick 'mickrip' Rippon"
 aboutgui = CreateGui() 
 
@@ -8,7 +9,8 @@ aboutgui = CreateGui()
 abl = CreateLabel(aboutgui, "AboutLabel", 90, 200, abouttext)
 SetLabelFont(abl, font)
 
--- Add a new Back Buttion
+-- Add a new Back Button texture
+-- TODO: Add this to a seperate file, so it only gets loaded once.
 bbtexoff = CreateTextureFromFile("BackButtonOn","resources/menu/back_off.png")
 bbtexover = CreateTextureFromFile("BackButtonHover", "resources/menu/back_hover.png")
 bbtexon = CreateTextureFromFile("BackButtonClick", "resources/menu/back_off.png")
@@ -19,4 +21,6 @@ SetButtonOverTexture(backButton,bbtexover)
 SetButtonOnTexture(backButton, bbtexon)
 CallBack.Create(backButton, "BackScript", "SetGui(maingui);PlaySound(menu_click, 0.5)")
 
+-- Show the menu after all the
+-- menu resources have been loaded.
 SetGui(aboutgui)

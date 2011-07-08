afdc9420f74492ce08905ddb1d9b82af0eb96f2e
Mark Hahl <Wolskie@hotmail.com>
2011-07-07 21:51:05 -0700

    Updated comments and added TODO information.

--- resources/script/functions.lua
+++ resources/script/functions.lua
@@ -4,8 +4,8 @@
       Change what works :\
 --]]
 
+-- TODO: Remove this and add a script for each button press.
 CallBack = {}
-
 function CallBack.Create(button, name, code)
 
     print("In function CallBack.Create()")
@@ -24,20 +24,28 @@ function Game.Load(level_pak)
   scene = LoadScene(level_pak)
   cam = GetSceneActiveCamera(scene)
   
+  -- Set the physics of the camera to a box and change:
+  -- * FOV to 90
+  -- * SHAPE to BOX
+  -- * The MASS to 20
   SetActorPhysics(cam, true)
   SetActorShape(cam, BOX)
   SetCameraFov(cam, 90)
   SetActorRestitution(cam, 0.0)
   SetActorMass(cam, 20)
   
+  -- Debug information.
+  -- TODO: Only print this dort of information when debugging is enabled.
   print("......... Entity Information............")
   print(GetSceneEntityCount(scene))
   print(GetSceneEntity(scene, "Plane") )
 
-  
+  -- Level is loaded, start polling for events in the main loop.
   level_loaded = true
   HideMouse( true )
   SetGuiObjectVisible(maingui, false )
+
+  -- Set the GUI to a blank gui.
   SetGui(blankgui)
   
 end
